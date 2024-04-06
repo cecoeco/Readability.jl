97809e8d71bdec29803d380dd41e45c7c59ff2d3
webofceco <mapleselijah9@gmail.com>
2024-04-06 19:42:50 -0400

    automerge fix?

--- src/gunning_fog.jl
+++ src/gunning_fog.jl
@@ -3,8 +3,8 @@ function GunningFog(text::String)
     total_sentences::Int = sentences(text)
     total_complex_words::Int = complex_words(text)
 
-    words_per_sentence::Float64 = (total_words / total_sentences)
-    percentage_of_complex_words::Float64 = 100 * (total_complex_words / total_words)
+    words_per_sentence::Float64 = total_words / total_sentences
+    percentage_of_complex_words::Float64 = 100 * total_complex_words / total_words
 
     fog_index::Float64 = 0.4 * (words_per_sentence + percentage_of_complex_words)
     return fog_index
