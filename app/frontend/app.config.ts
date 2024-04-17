a0d061f1f4211a5441903fa88ed1b416b9d73945
Ceco Maples <108555136+cecoeco@users.noreply.github.com>
2024-04-17 16:18:41 -0400

    Update app.config.ts

--- app/frontend/app.config.ts
+++ app/frontend/app.config.ts
@@ -1,3 +1,10 @@
-import { defineConfig } from "@solidjs/start/config";
-
-export default defineConfig({});
+import vercel from "solid-start-vercel";
+import solid from "solid-start/vite";
+import { defineConfig } from "vite";
+export default defineConfig({
+  plugins: [
+    solid({
+      adapter: vercel()
+    })
+  ]
+});
