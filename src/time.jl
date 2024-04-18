1ce89146c1f53978812b672b508bc2d577b836a1
webofceco <mapleselijah9@gmail.com>
2024-04-18 00:04:04 -0400

    fixed docstrings

--- src/time.jl
+++ src/time.jl
@@ -1,7 +1,7 @@
 """
     reading_time(text::String; wpm::Number=238)
 
-Returns the reading time of `text` in minutes.
+Returns the reading time of `text` in seconds.
 """
 function reading_time(text::String; wpm::Number=238)
     total_words::Int = words(text)
@@ -12,7 +12,7 @@ end
 """
     speaking_time(text::String; wpm::Number=183)
 
-Returns the speaking time of `text` in minutes.
+Returns the speaking time of `text` in seconds.
 """
 function speaking_time(text::String; wpm::Number=183)
     total_words::Int = words(text)
